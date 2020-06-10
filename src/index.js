517b1af78d49e4c42652a1b79a94e757e8f55e34
samboudjebla <samboudjebla@hotmail.com>
2020-06-10 15:15:39 -0700

    created a clicker counter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let model = { clicks: 0 };
 
+
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App clicks={model.clicks} onClick={() => { model.clicks += 1; render(); }} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+};
+
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
